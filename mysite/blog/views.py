7dc038cecd94830a32a833903135c599c572308b
npeyre <90184564+npeyre@users.noreply.github.com>
2026-06-22 21:03:07 +0100

    Added a view to display a single post

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,4 +6,16 @@ def post_list(request):
     posts = Post.published.all()
     return render(request, 
                   'blog/post/list.html', 
-                  {'posts' : posts})
+                  {'posts': posts})
+
+
+# A view to display a single post
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish_year=year,
+                             publish_month=month,
+                             publish_day=day)
+    return render(request, 
+                  'blog/post/detail.html',
+                  {'post': post})
