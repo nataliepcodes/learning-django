ff7abc061922f51592cf6f175448e8761afbb8af
npeyre <90184564+npeyre@users.noreply.github.com>
2026-06-29 19:03:10 +0100

    Added tagging option

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,18 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 # A view to retrieve all published posts
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        # __in field lookup; many-to-many relationship - one post can have multiple tags, multiple tags can related to multiple posts
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list,3) # 3 posts on each page
     page = request.GET.get('page')
     try:
@@ -19,7 +27,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request,'blog/post/list.html',
                   {'page': page, 
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 # A view to display a single post
