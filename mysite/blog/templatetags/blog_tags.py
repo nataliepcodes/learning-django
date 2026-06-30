a596db81c2c054ef3119ece85bb46f169f0b0e18
npeyre <90184564+npeyre@users.noreply.github.com>
2026-06-30 15:17:57 +0100

    Added get_most_commented_posts function

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from ..models import Post
+from django.db.models import Count
 
 register = template.Library()
 
@@ -24,3 +25,8 @@ How it works:
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts' : latest_posts} 
+
+# A tag to display the most commented post
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
