c5dc588ce21f138dc3edfdfb81cad0def501f501
npeyre <90184564+npeyre@users.noreply.github.com>
2026-06-30 13:49:22 +0100

    Added tag for latest posts

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -3,7 +3,13 @@ from ..models import Post
 
 register = template.Library()
 
-# Retrieve total blog posts published
+# Retrieve total blog posts published / Displayed on the blog sidebar
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+# Display latest posts / Displayed on the blog sidebar
+@register.inclusion_tag('blog/post/latest)posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts' : latest_posts}
