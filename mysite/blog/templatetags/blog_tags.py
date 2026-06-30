bac22b1ad1d1f6e2c47cb2701f5ca48882cadd4a
npeyre <90184564+npeyre@users.noreply.github.com>
2026-06-30 13:41:15 +0100

    Updated to fix errors

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -4,6 +4,6 @@ from ..models import Post
 register = template.Library()
 
 # Retrieve total blog posts published
-@register.simple_tags
+@register.simple_tag
 def total_posts():
     return Post.published.count()
