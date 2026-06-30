0d83b01ee70b59797440188aaa1fd8abc1016913
npeyre <90184564+npeyre@users.noreply.github.com>
2026-06-30 14:28:20 +0100

    Added how the function works

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -9,7 +9,18 @@ def total_posts():
     return Post.published.count()
 
 # Display latest posts / Displayed on the blog sidebar
+"""
+How it works:
+0. There are 2 html templates (base.html and latest_posts.html) and 1 py file (blog_tags.py)
+1. Django will call latest posts show_latest_posts() in blog_tags.py, the data is asked from base.html that is asking for 3 latests posts - {% show_latest_posts 3 %}
+2. show_latest_posts() in the below function will get the latest posts from the Post class and will return/output the data 
+3. The return data will be used in latest_posts.html that is registered in the inclusion tag of show_latest_posts()
+3. latest_posts.html has a loop that iterates over a dictionary of values
+4. What's a link between base.html and latest_posts.html? 
+- {% show_latest_posts 3 %} in base.html is a shortcut to include another template, in this case latest_posts.html which is inluded at the registration step of show_latest_posts()
+- the show_latest_posts() return data is first processed within the loop in latest_posts.html, then the final result is displayed in the base.html
+"""
 @register.inclusion_tag('blog/post/latest_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts' : latest_posts} # Data is received from blog/post/latest_posts.html
+    return {'latest_posts' : latest_posts} 
