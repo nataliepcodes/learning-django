429afb4742ee2949d6d1d34f0c97f49f897a01bc
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-01 21:08:48 +0100

    Added seach function

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+
 
 # A view to retrieve all published posts
 def post_list(request, tag_slug=None):
@@ -74,6 +76,7 @@ def post_detail(request, year, month, day, post):
                    'comment_form': comment_form,
                    'similar_posts': similar_posts})
 
+
 # A view to share a post by email
 def post_share(request, post_id):
     # Retrieve post by id
@@ -97,3 +100,21 @@ def post_share(request, post_id):
         form = EmailPostForm()
     
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+# Text Search View
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+
+    return render(request, 'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})        
+
