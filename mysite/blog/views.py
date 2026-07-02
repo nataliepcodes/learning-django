b71519964ba5cd3162d5ca0970481387aad6c033
npeyre <90184564+npeyre@users.noreply.github.com>
2026-07-02 09:17:00 +0100

    Updated with a missing line and added spaces

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -111,10 +111,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             # Create search query object
+            query = form.cleaned_data['query']
             search_vector = SearchVector('title', 'body')
             search_query = SearchQuery(query)
             # Filter results by SearchQuery, use SearchRank to order results. Order by relevancy
-            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector,search_query)).filter(search=search_query).order_by('-rank')
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
 
     return render(request, 'blog/post/search.html',
                   {'form': form,
